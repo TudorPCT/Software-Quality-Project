c98a4e2ce824e3ae8acabbd23a13279252581a39
Radu Stefan MIHALACHE <rmihalache@bitdefender.com>
2024-04-30 10:20:45 +0300

    done config

--- main_gui.py
+++ main_gui.py
@@ -5,8 +5,11 @@ from memory.main_memory import MainMemory
 from memory.program_memory import ProgramMemory
 from processor.processor import Processor
 from data_type.int8 import Int8
+from data_type.int16 import Int16
 import time
 from threading import Thread
+
+from util.config_parser import get_data
 
 
 def computer_run():
@@ -24,6 +27,8 @@ def computer_run():
 
 
 if __name__ == "__main__":
+    config_data = get_data(r"D:\personal\Quality\Software-Quality-Project\resources\config\config_alabul.txt")
+
     gui = ComputerGUI()
 
     keyboard_perf = KeyboardPeripheral()
@@ -32,8 +37,8 @@ if __name__ == "__main__":
     screen_perf.configure_gui(gui)
     keyboard_perf.configure_gui(gui)
 
-    main_memory = MainMemory(1024, [keyboard_perf, screen_perf])
-    program_memory = ProgramMemory(128)
+    main_memory = MainMemory(config_data["main_memory_size"], [keyboard_perf, screen_perf], [Int16(config_data["keyboard_address"]), Int16(config_data["screen_address"])], Int16(config_data["stack_address"]))
+    program_memory = ProgramMemory(config_data["program_memory_size"])
 
     cpu = Processor(main_memory, program_memory)
 
