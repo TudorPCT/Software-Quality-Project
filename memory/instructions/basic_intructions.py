46359a5d1a28f2ca195a4fca56a5d0fbe354998c
TudorPCT <tcosmin.pasat@gmail.com>
2024-05-07 18:35:35 +0300

    revert

--- memory/instructions/basic_intructions.py
+++ memory/instructions/basic_intructions.py
@@ -86,17 +86,24 @@ class Mul(BasicInstruction):
         return lh * rh
 
 
-class Div(BasicInstruction):
+class Div(Instruction):
     """
-    div eax, ebx
-    div eax, 10
-    div [10], ecx
+    div ebx
+    div [10]
     """
-    def __init__(self, lh: Operand, rh: Operand):
-        super().__init__(lh, rh)
 
-    def operation(self, lh: Int16, rh: Int16) -> Int16:
-        return lh / rh
+    def __init__(self, rh: Operand):
+        super().__init__()
+        self.rh = rh
+
+    def run(self, cpu: Processor) -> None:
+        result = cpu.register_eax / self.rh
+        remainder = cpu.register_eax % self.rh
+
+        set_value(cpu, Operand(Register["eax"]), result)
+        set_value(cpu, self.rh, remainder)
+
+        self.end(cpu)
 
 
 class NOT(Instruction):
