72bd6a8a245d4b81f8d298e4e94376a9e8423caa
TudorPCT <tcosmin.pasat@gmail.com>
2024-05-14 21:03:44 +0300

    Instructions parser tests

---
+++ tests/processor/read_instrucitons/test_read_instructions.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from src.processor.read_instructions import InstructionParser
+
+class TestReadInstructions(TestCase):
+
+    @patch('builtins.open')
+    def test_read_instructions_from_file(self, mock_open):
+        mock_file_content = [
+            "label1:",
+            "mov [10], eax",
+            "label2:",
+            "jmp label1"
+        ]
+
+        mock_file = MagicMock()
+        mock_file.__enter__.return_value.__iter__.return_value = mock_file_content
+        mock_open.return_value = mock_file
+
+        instruction_parser = InstructionParser("test.txt")
+        instruction_parser.parse_instruction = MagicMock(return_value="mocked_instruction")
+
+        instructions = instruction_parser.read_instructions_from_file()
+
+        self.assertEqual(len(instructions), 2)
+        self.assertEqual(instructions[0], "mocked_instruction")
+        self.assertEqual(instructions[1], "mocked_instruction")
